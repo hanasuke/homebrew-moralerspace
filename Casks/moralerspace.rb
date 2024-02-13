eb55c4d76b5c6c69f0499b05fe14060f8d642d1c
naosuke <info@naosuke.me>
2024-02-14 02:11:06 +0900

    add moralerspace definition

---
+++ Casks/moralerspace.rb
@@ -0,0 +1,30 @@
+cask 'moralerspace' do
+  version '0.0.5'
+  sha256 '067a08866c788db0ba9133ba139d2eb3750f6f2253a43bdd0e3418f57e0d9c10'
+
+  url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/Moralerspace_v#{version}.zip"
+  name 'Moralerspace'
+  homepage 'https://github.com/yuru7/moralerspace'
+  desc 'Moralerspace は、欧文フォント Monaspace と日本語フォント IBM Plex Sans JP などを合成したプログラミング向けフォントです。'
+
+  font "Moralerspace_v#{version}/MoralerspaceArgon-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceArgon-Regular.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceKrypton-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceKrypton-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceKrypton-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceKrypton-Regular.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceNeon-Regular.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceRadon-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceRadon-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceRadon-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceRadon-Regular.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceXenon-Bold.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceXenon-BoldItalic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceXenon-Italic.ttf"
+  font "Moralerspace_v#{version}/MoralerspaceXenon-Regular.ttf"
+end
