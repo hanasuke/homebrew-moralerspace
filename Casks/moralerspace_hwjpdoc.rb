5daee5471605f36e3672b3bff3136ae76da913e8
naosuke <info@naosuke.me>
2024-02-14 02:39:37 +0900

    add moralerspace HWJPDOC definition

---
+++ Casks/moralerspace_hwjpdoc.rb
@@ -0,0 +1,30 @@
+cask 'font-moralerspace_hwjpdoc' do
+  version '0.0.5'
+  sha256 'c10d84a715bebc56795186557c1fe772b6a8cc6c9ea701ad43a7e649c5a7c51b'
+
+  url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/MoralerspaceHWJPDOC_v#{version}.zip"
+  name 'MoralerspaceJPDOC'
+  homepage 'https://github.com/yuru7/moralerspace'
+  desc 'Moralerspace は、欧文フォント Monaspace と日本語フォント IBM Plex Sans JP などを合成したプログラミング向けフォントです。'
+
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceArgonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceKryptonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceNeonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceRadonHWJPDOC-Regular.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-Bold.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-BoldItalic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-Italic.ttf"
+  font "MoralerspaceHWJPDOC_v#{version}/MoralerspaceXenonHWJPDOC-Regular.ttf"
+end
